bf04bcc7c82bd736e2afdda5b969624dbecba96d
Ashley-Blu <matsekolengashley28@gmail.com>
2025-08-01 14:52:58 +0200

    Added html document, css, inpiration document, wireframe

---
+++ App.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+   
+    const homebtn = document.getElementById('homebtn');
+    const binarybtn = document.getElementById('binarybtn')
+    const decimalbtn = document.getElementById
+})
